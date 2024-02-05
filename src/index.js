1ef24291a7dbfbc152d6bcf8e9b4809d0b2fcf92
AlphaZer00 <batudasdemir@gmail.com>
2024-02-05 12:04:48 -0500

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 // https://webpack.js.org/guides/getting-started/
-import myName from './myName';
-import './style.css';
-import Icon from './icon.png';
-import Books from './books.xml';
+import _ from 'lodash';
+import printMe from './print.js';
+
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
+
   
-    element.textContent = myName('Batuhan');
-    element.classList.add('hello');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.table(Books)
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
 
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,15 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-    ],
-  },
   plugins: [
     new HTMLWebpackPlugin({
       title: 'Development',
